6485b0a7fc27c5ea3ad3ed39fdd4042ef5023589
Jkorf <jankorf91@gmail.com>
2021-09-02 10:16:07 +0200

    Added base class for FTXOrder/FTXTriggerOrder

--- FTX.Net/Objects/Spot/FTXOrder.cs
+++ FTX.Net/Objects/Spot/FTXOrder.cs
@@ -1,6 +1,7 @@
 ﻿using CryptoExchange.Net.ExchangeInterfaces;
 using FTX.Net.Converters;
 using FTX.Net.Enums;
+using FTX.Net.Objects.Spot;
 using Newtonsoft.Json;
 using System;
 
@@ -9,63 +10,31 @@ namespace FTX.Net.Objects
     /// <summary>
     /// Order info
     /// </summary>
-    public class FTXOrder: ICommonOrderId, ICommonOrder
+    public class FTXOrder: FTXOrderBase, ICommonOrderId, ICommonOrder
     {
         /// <summary>
-        /// Id of the order
+        /// The order type
         /// </summary>
-        public long Id { get; set; }
-        /// <summary>
-        /// When the order was created
-        /// </summary>
-        public DateTime CreatedAt { get; set; }
-        /// <summary>
-        /// The quantity that is filled
-        /// </summary>
-        [JsonProperty("filledSize")]
-        public decimal FilledQuantity { get; set; }
-        /// <summary>
-        /// The future the order is for
-        /// </summary>
-        public string Future { get; set; } = string.Empty;
-        /// <summary>
-        /// The symbol to order is for
-        /// </summary>
-        [JsonProperty("market")]
-        public string Symbol { get; set; } = string.Empty;
+        [JsonConverter(typeof(OrderTypeConverter))]
+        [JsonProperty("type")]
+        public override OrderType OrderType { get; set; }
+
         /// <summary>
         /// The price of the order
         /// </summary>
-        public decimal? Price { get; set; }
+        [JsonProperty("price")]
+        public override decimal? OrderPrice { get; set; }
+
         /// <summary>
         /// The remaining quantity
         /// </summary>
         [JsonProperty("remainingSize")]
         public decimal RemainingQuantity { get; set; }
         /// <summary>
-        /// The side
-        /// </summary>
-        [JsonConverter(typeof(OrderSideConverter))]
-        public OrderSide Side { get; set; }
-        /// <summary>
-        /// The order type
-        /// </summary>
-        [JsonConverter(typeof(OrderTypeConverter))]
-        public OrderType Type { get; set; }
-        /// <summary>
-        /// The total quantity of the order
-        /// </summary>
-        [JsonProperty("size")]
-        public decimal Quantity { get; set; }
-        /// <summary>
         /// The status of the order
         /// </summary>
         [JsonConverter(typeof(OrderStatusConverter))]
         public OrderStatus Status { get; set; }
-        /// <summary>
-        /// Reduce only order
-        /// </summary>
-        public bool ReduceOnly { get; set; }
         /// <summary>
         /// Immediate or cancel order
         /// </summary>
@@ -79,15 +48,10 @@ namespace FTX.Net.Objects
         /// Client id
         /// </summary>
         public string? ClientId { get; set; }
-        /// <summary>
-        /// Average execution price
-        /// </summary>
-        [JsonProperty("avgFillPrice")]
-        public decimal? AverageFillPrice { get; set; }
 
         string ICommonOrder.CommonSymbol => Symbol;
 
-        decimal ICommonOrder.CommonPrice => Price ?? 0;
+        decimal ICommonOrder.CommonPrice => OrderPrice ?? 0;
 
         decimal ICommonOrder.CommonQuantity => Quantity;
 
@@ -112,7 +76,7 @@ namespace FTX.Net.Objects
 
         IExchangeClient.OrderSide ICommonOrder.CommonSide => Side == OrderSide.Buy ? IExchangeClient.OrderSide.Buy : IExchangeClient.OrderSide.Sell;
 
-        IExchangeClient.OrderType ICommonOrder.CommonType => Type == OrderType.Limit ? IExchangeClient.OrderType.Limit : IExchangeClient.OrderType.Market;
+        IExchangeClient.OrderType ICommonOrder.CommonType => OrderType == OrderType.Limit ? IExchangeClient.OrderType.Limit : IExchangeClient.OrderType.Market;
 
         DateTime ICommonOrder.CommonOrderTime => CreatedAt;
 
